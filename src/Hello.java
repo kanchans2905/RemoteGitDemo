591b0652845b15e63ffbbc6fe60aa88c60e3d941
kanchans2905 <Dell@DESKTOP-KJ6RT1R>
2019-03-23 16:03:32 +0530

    feature 2 commit

--- src/Hello.java
+++ src/Hello.java
@@ -4,10 +4,12 @@ public class Hello {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 System.out.println("hello git");
-System.out.println("second commit");
+System.out.println("new commit commit");
 System.out.println("devloper changed");
 //changes
 System.out.println("changes ");
+//changes in fearture2
+System.out.println(" this is feature 2");
 	}
 
 }
